b151819d21bc30992a0f5e9c13dbff9ad23467c0
Markus Westerlind <marwes91@gmail.com>
2018-02-18 11:46:21 +0100

    Disable incremental compilation for wasm

--- wasm.sh
+++ wasm.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-cargo build -p gluon_wasm --release --target wasm32-unknown-unknown
+CARGO_INCREMENTAL=0 cargo build -p gluon_wasm --release --target wasm32-unknown-unknown
 wasm-bindgen target/wasm32-unknown-unknown/release/gluon_wasm.wasm --out-dir src/client/
 (cd src/client && wasm2es6js gluon_wasm_wasm.wasm -o gluon_wasm_wasm.js --base64)
 parcel src/client/index.html
